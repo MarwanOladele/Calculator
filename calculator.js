338d99f17fbe9591bc290184de9c5d5cc392fa57
unknown <you@example.com>
2021-10-11 20:53:54 +0100

    new commit

--- calculator.js
+++ calculator.js
@@ -15,12 +15,13 @@ buttons.map((button) => {
         display.innerText = display.innerText.slice(0, -4);
       } else if (e.target.innerText == "=") {
         display.innerText = display.innerText.slice(0, -1);
-        display.innerText = eval(display.innerText).toFixed(3);
+        display.innerText = eval(display.innerText);
+        if (display.innerText.length > 12) {
+          display.innerText = eval(display.innerText).toFixed(3);
+        }
       }
     } catch (error) {
       display.innerText = "Syn Error";
     }
   });
 });
-
-
