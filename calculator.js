15432f4a1ba0d1da8f1f5e8e665355dd8cdd4517
unknown <you@example.com>
2021-10-11 15:27:29 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("btn"));
+
+buttons.map((button) => {
+  button.addEventListener("click", function (e) {
+    display.innerText += e.target.innerText;
+    try {
+      if (display.innerText.length > 14) {
+        alert("Maximum Input");
+        display.innerText = "";
+      } else if (e.target.innerText == "AC") {
+        display.innerHTML = "";
+      } else if (e.target.innerText == "DEL") {
+        display.innerText = display.innerText.slice(0, -4);
+      } else if (e.target.innerText == "=") {
+        display.innerText = display.innerText.slice(0, -1);
+        display.innerText = eval(display.innerText);
+      }
+    } catch (error) {
+      display.innerText = "Syntax Error";
+    }
+  });
+});
+
+
